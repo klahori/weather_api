c8c986ee13dcd009c199bce4bcdb522e549bdcf3
klahori <klaho028@uottawa.ca>
2020-08-15 22:54:57 -0400

    Updates

---
+++ precache-manifest.dd9594e023ea0d1a754007176311aee3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1856035e3ca3a2d992301f9eefea36f",
+    "url": "/weather_api/index.html"
+  },
+  {
+    "revision": "c66ca312a80eaed53d73",
+    "url": "/weather_api/static/css/main.e82a1ae1.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/weather_api/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_api/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66ca312a80eaed53d73",
+    "url": "/weather_api/static/js/main.7c91d605.chunk.js"
+  },
+  {
+    "revision": "e91cc54d4fa80632f811",
+    "url": "/weather_api/static/js/runtime-main.cb3aee54.js"
+  },
+  {
+    "revision": "0fb668e30adc36a8df2e4e3bef2a4367",
+    "url": "/weather_api/static/media/cold.0fb668e3.jpg"
+  },
+  {
+    "revision": "8b69ef19bc2e0ae7e2d6a270cd2d35b7",
+    "url": "/weather_api/static/media/warm.8b69ef19.png"
+  }
+]);
